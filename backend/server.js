60126485780ec9630bc6c2165e0847d11b79c710
Patima Poochai <ppoochai@hawaii.edu>
2020-06-07 19:47:23 -1000

    Fix CLI issues for backend

--- backend/server.js
+++ backend/server.js
@@ -5,21 +5,22 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
-const app = express()
+const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
 
-const connection = mongoose.connection;
+/* es lint asks for destructuring here, remove comment as necessary */
+const connection = mongoose.connection; // eslint-disable-line
 
 connection.once('open', () => {
-    console.log("MongoDB database connection established successfully");
-})
+  console.log('MongoDB database connection established successfully');
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`)
-})
+  console.log(`Server is running on port: ${port}`);
+});
