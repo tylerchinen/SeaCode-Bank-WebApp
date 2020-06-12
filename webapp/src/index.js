77140ae0a76681b0adfe8be4fa4731bac8fad090
Tyler Chinen <tlc852@hawaii.edu>
2020-06-11 16:06:08 -1000

    added serviceworker import

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
-
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
+    <React.StrictMode>
       <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
