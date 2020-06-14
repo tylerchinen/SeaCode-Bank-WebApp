35c321b1a156eea26484fcf30e49d51210550ed0
Tyler Chinen <tlc852@hawaii.edu>
2020-06-13 23:41:00 -1000

    login html page in-progress

--- webapp/public/login.js
+++ webapp/public/login.js
@@ -1,13 +1,6 @@
 import React from 'react'
 import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
 
-class Login extends React.Component {
-  render() {
-
-  }
-}
- export default Login;
-
   const LoginForm = () => (
       <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
         <Grid.Column style={{ maxWidth: 450 }}>
