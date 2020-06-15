c94356ddb15712ad79d4150dfc513b29e5646182
Patima Poochai <ppoochai@hawaii.edu>
2020-06-14 21:25:05 -1000

    Change seed data to be encrpyted

--- backend/config/seedData.js
+++ backend/config/seedData.js
@@ -2,9 +2,9 @@ const User = require('../models/user');
 
 const users = [
   new User({
-    username: 'test',
-    password: 'testPass',
-    email: 'test@gmail.com',
+    username: 'john',
+    password: '$2b$10$yDmQF9vJfqmjdsl9TaBHIewpnjck1vId/avmLHrHp6vmPJbSlH3xW',
+    email: 'john@gmail.com',
   }),
 ];
 
