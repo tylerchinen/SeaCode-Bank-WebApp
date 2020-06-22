fd4811bee09c3b3afff59e382a43c979e160d8f8
miraabela <mirabela@hawaii.edu>
2020-06-21 22:56:45 -1000

    wire

--- webapp/src/components/History.js
+++ webapp/src/components/History.js
@@ -7,9 +7,8 @@ export default class History extends React.Component {
         return (
             <Container textAlign='center' fluid style ={{backgroundImage: `url(https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&w=1000&q=80)`,
             height: '100vh', backgroundSize: 'cover'}}>
-                <Header style={{paddingTop: '10px'}} as='h1'>History</Header>
-                <Segment style ={{marginLeft: '100px', marginRight: '100px'}}>
                 <Header style={{paddingTop: '10px'}} as='h1'>Account History</Header>
+                <Segment style ={{marginLeft: '100px', marginRight: '100px'}}>
                 <Card.Group>
                     <Card style={{marginTop: '-2px'}} fluid color='red' header='Deposit Check $500' meta='June 18, 2020 2:40 PM'/>
                     <Card style={{marginTop: '-2px'}} fluid color='orange' header='Withdraw Cash $200' meta='June 20, 2020 3:30 PM'/>
--- webapp/src/components/Wire.js
+++ webapp/src/components/Wire.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { Container, Segment, Header } from 'semantic-ui-react';
+import { Container, Segment, Header, Form, Button, Input } from 'semantic-ui-react';
 
 export default class Wire extends React.Component {
     render() {
@@ -9,7 +9,17 @@ export default class Wire extends React.Component {
             height: '100vh', backgroundSize: 'cover'}}>
                 <Header style={{paddingTop: '10px'}} as='h1'>Wire Funds</Header>
                 <Segment style ={{marginLeft: '100px', marginRight: '100px'}}>
-
+                <Form>
+                    <Form.Field>
+                    <Form.Input fluid label='Email of Recieving Party' placeholder='Email Address' />
+                    </Form.Field>
+                    <Form.Field>
+                    <Form.Input fluid label='Amount' placeholder='Enter an Amount' />
+                    </Form.Field>
+                    <Form.Field>
+                    </Form.Field>
+                    <Button type='submit'>Submit</Button>
+                </Form>
                 </Segment>
             </Container>
         )
