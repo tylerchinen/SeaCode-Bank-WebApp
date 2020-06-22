bd553ad3d6542ada5dfebab27110175a23451d05
miraabela <mirabela@hawaii.edu>
2020-06-21 22:31:24 -1000

    Update History.js

--- webapp/src/components/History.js
+++ webapp/src/components/History.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { Container, Segment, Header } from 'semantic-ui-react';
+import { Container, Segment, Header, Card, Pagination} from 'semantic-ui-react';
 
 export default class History extends React.Component {
     render() {
@@ -9,7 +9,13 @@ export default class History extends React.Component {
             height: '100vh', backgroundSize: 'cover'}}>
                 <Header style={{paddingTop: '10px'}} as='h1'>History</Header>
                 <Segment style ={{marginLeft: '100px', marginRight: '100px'}}>
-
+                <Header style={{paddingTop: '10px'}} as='h1'>Account History</Header>
+                <Card.Group>
+                    <Card style={{marginTop: '-2px'}} fluid color='red' header='Deposit Check $500' meta='June 18, 2020 2:40 PM'/>
+                    <Card style={{marginTop: '-2px'}} fluid color='orange' header='Withdraw Cash $200' meta='June 20, 2020 3:30 PM'/>
+                    <Card style={{marginTop: '-2px'}} fluid color='yellow' header='Withdraw Cash $200 3' meta='June 22, 2020 5:20 PM'/>
+                </Card.Group>
+                <Pagination style={{marginTop: '10px'}} defaultActivePage={1} totalPages={2} />
                 </Segment>
             </Container>
         )
