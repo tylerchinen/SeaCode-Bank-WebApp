0a501eae813fd66e447a2f5b84dd759608a4f34c
miraabela <mirabela@hawaii.edu>
2020-07-03 12:57:49 -1000

    logout and deposit

--- webapp/src/components/Deposit.js
+++ webapp/src/components/Deposit.js
@@ -1,13 +1,77 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { Container, Segment, Header, Checkbox, Form, Button, Input } from 'semantic-ui-react';
+import { Container, Segment, Header, Checkbox, Form, Button, Input, Dimmer, Loader } from 'semantic-ui-react';
 
 export default class Deposit extends React.Component {
-    state = {}
-    handleChange = (e, { value }) => this.setState({ value })
-  
+    constructor(props) {
+        super(props);
+        this.state = {
+          modalOpen: false,
+          amount: 0,
+          submitError: false,
+          success: false,
+          lastRequest: 0,
+          withdrawLoading: false,
+          sessionLoading: true,
+          loggedIn: false,
+        }
+        this.loginCheck();
+      }
+      handleChange = (e, { name, value }) => {
+        this.setState({ [name]: value });
+      }
+    loginCheck() {
+        fetch('http://localhost:5000/api/users/sessioncheck', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          credentials: 'include',
+        }).then((response) => {
+            if (response.ok) {
+              this.setState({ loggedIn: true });
+            } else {
+              this.setState({ loggedIn: false });
+            }
     
-    render() {
+          this.setState({ sessionLoading: false });
+          }
+        );
+      }
+
+    handleProceed = () => {
+        const amount  = this.state.amount;
+        this.setState({
+          submitError: false,
+          withdrawLoading: true,
+        });
+        fetch('http://localhost:5000/api/users/protected/deposit', {
+          method: 'POST',
+          body: JSON.stringify({ amount: amount }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          credentials: 'include',
+        }).then((response) => {
+            if (response.ok) {
+              this.setState({
+                success: true,
+                lastRequest: amount,
+              });
+            } else {
+              this.setState({
+                submitError: true,
+              });
+            }
+            this.setState({
+              modalOpen: false,
+              withdrawLoading: false,
+            });
+          }
+        );
+      };
+    
+      renderContent() {
         return (
             <Container textAlign='center' fluid style ={{backgroundImage: `url(https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&w=1000&q=80)`,
             height: '100vh', backgroundSize: 'cover'}}>
@@ -40,10 +104,31 @@ export default class Deposit extends React.Component {
                     </Form.Field>
                 </Form>
                 <Header style={{paddingTop: '10px'}} as='h3'>Step 2: Amount</Header>
-                <Input focus placeholder='Enter an amount' />
-                <Button>Confirm</Button>
+                <Input focus placeholder='Enter an amount' name='amount' onChange={this.handleChange} />
+                <Button onClick={this.handleProceed}>Confirm</Button>
                 </Segment>
             </Container>
         )
     }
+
+    render() {
+        if (this.state.sessionLoading) {
+          return (
+            <Dimmer active inverted>
+              <Loader inverted>Loading</Loader>
+            </Dimmer>
+          );
+        }
+    
+        return (this.state.loggedIn)
+          ? this.renderContent()
+          : <Container textAlign='center' fluid style={{
+            backgroundImage: `url(https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&w=1000&q=80)`,
+            height: '100vh',
+            backgroundSize: 'cover',
+          }}>
+            <Header as='h1' content='Login Required'/>
+            <Header as='h2' content='Please log in'/>
+          </Container>;
+      }
 }
--- webapp/src/components/Navbar.js
+++ webapp/src/components/Navbar.js
@@ -1,11 +1,60 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import {Menu, Dropdown, Header} from 'semantic-ui-react';
+import {Menu, Dropdown, Header, Dimmer, Loader, Message} from 'semantic-ui-react';
 
 export default class Navbar extends React.Component {
-    render() {
+    constructor(props) {
+        super(props);
+        this.state = {
+          doneLoading: false,
+          loggedIn: false,
+          firstname: '',
+          lastname: '',
+          balance: '',
+          logoutError: false,
+        }
+      }
+
+      errorDisplay() {
+        if (this.state.logoutError) {
+          return (
+            <Message
+              negative
+              header='Unable to Login'
+              content='Invalid email address or password'
+            />
+          );
+        }
+      }
+
+      submit = () => {
+        this.setState({ logoutError: false});
+        fetch('http://localhost:5000/api/users/logout', {
+          method: 'GET',
+          }).then((response) => {
+            if (response.ok) {
+              this.props.history.push('/');
+            } else {
+              this.setState({ logoutError: true });
+            }
+          }
+        );
+      }
+
+    renderContent() {
         return (
             <Menu style={{ paddingBottom: '15px' }} attached="top" borderless inverted>
+                <Menu.Item as={NavLink} exact to="/" key='home'>Home</Menu.Item>
+                <Menu.Item as={NavLink} exact to="/dashboard" key='dashboard'>Dashboard</Menu.Item>
+                <Menu.Item position="right">
+                <Menu.Item position="right" text="Log out" onItemClick={this.submit} key='dashboard'>Log Out</Menu.Item>
+                </Menu.Item>
+            </Menu>
+        )
+    }
+    render() {
+        return (this.state.loggedin) ? this.renderContent() 
+        :   <Menu style={{ paddingBottom: '15px' }} attached="top" borderless inverted>
                 <Menu.Item as={NavLink} exact to="/" key='home'>Home</Menu.Item>
                 <Menu.Item as={NavLink} exact to="/dashboard" key='dashboard'>Dashboard</Menu.Item>
                 <Menu.Item position="right">
@@ -17,6 +66,5 @@ export default class Navbar extends React.Component {
                     </Dropdown>
                 </Menu.Item>
             </Menu>
-        )
     }
 }
